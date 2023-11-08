195127ad2b04c7bbb3b06327fab97f54db459fa8
gabreudev <biel0alves0@gmail.com>
2023-11-08 11:08:10 -0200

    feat: initial commit

---
+++ src/main/java/com/virtex/testeVirtex/TesteVirtexApplication.java
@@ -0,0 +1,13 @@
+package com.virtex.testeVirtex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteVirtexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteVirtexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virtex/testeVirtex/TesteVirtexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtex.testeVirtex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteVirtexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
