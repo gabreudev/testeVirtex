d718051dab12c73b4b86402ec13f3cbd3ab2ccea
gabreudev <biel0alves0@gmail.com>
2023-11-12 17:54:50 -0200

    feat: configurando rotas da api

--- src/main/java/com/virtex/testeVirtex/Controllers/ONUController.java
+++ src/main/java/com/virtex/testeVirtex/Controllers/ONUController.java
@@ -5,22 +5,28 @@ import com.virtex.testeVirtex.Servicies.ONUService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/ONU")
 public class ONUController {
  private final ONUService service;
 
+ @GetMapping
  public ResponseEntity<List<ONU>> listarONU(){
      List<ONU> lista = service.listarONU();
 
      return ResponseEntity.ok(lista);
  }
-    public ResponseEntity cadastrarONU(){
-        service.cadastrarOnus();
+ @PostMapping
+ public ResponseEntity cadastrarONU(){
+     service.cadastrarOnus();
 
-        return ResponseEntity.ok().build();
+     return ResponseEntity.ok().build();
     }
 }
