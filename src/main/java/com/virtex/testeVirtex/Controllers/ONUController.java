a429aed0f52d7605140c1a264b96f09eddcfffea
gabreudev <biel0alves0@gmail.com>
2023-11-12 21:58:43 -0200

    feat: documentando a api com sagger ui

--- src/main/java/com/virtex/testeVirtex/Controllers/ONUController.java
+++ src/main/java/com/virtex/testeVirtex/Controllers/ONUController.java
@@ -2,28 +2,32 @@ package com.virtex.testeVirtex.Controllers;
 
 import com.virtex.testeVirtex.Entities.ONU;
 import com.virtex.testeVirtex.Servicies.ONUService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 @RequestMapping("/ONU")
 public class ONUController {
  private final ONUService service;
 
- @GetMapping
+ @Operation(description = "lista as onus cadastradas no banco")
+ @GetMapping("/")
  public ResponseEntity<List<ONU>> listarONU(){
      List<ONU> lista = service.listarONU();
 
      return ResponseEntity.ok(lista);
  }
- @PostMapping
+ @Operation(description = "lê os dados do arquivo estático e cadastra no banco de dados")
+ @PostMapping("/")
  public ResponseEntity cadastrarONU(){
      service.cadastrarOnus();
 
